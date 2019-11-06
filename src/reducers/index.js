ee51306b9bf130e7672942a16d8abd8ada58898b
Sam <Jeffcoat>
2019-11-06 16:57:41 -0600

    Fixes another typo in reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -18,7 +18,7 @@ const initialState = {
     ]
 };
 
-export const reducer = (state = initialState. action) => {
+export const reducer = (state = initialState, action) => {
     console.log(action)
     switch(action.type){
         case ADD_FEATURE:
