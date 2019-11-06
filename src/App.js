c33eeaf06e59eaa948f87d63898c4709db8c30bc
Sam <Jeffcoat>
2019-11-06 15:08:55 -0600

    Imports Create store and creates initial state for our reducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+//step 1 import create store
+import { createStore } from "redux";
+
+
+// step 2 create our mock reducer- function that returns and object which returns updated 
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
@@ -6,22 +11,6 @@ import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
 const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
