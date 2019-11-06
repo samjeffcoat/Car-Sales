aec2573f0575de6636bfab00d3208bd21c3232fd
Sam <Jeffcoat>
2019-11-06 16:51:53 -0600

    fixes imports on index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from "react-redux";
-const store = createStore(reducer);
+import {reducer} from './reducers/index';
+import {createStore} from 'redux';
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
+const store = createStore(reducer)
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
